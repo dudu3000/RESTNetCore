9328dfd58ac5015bc4917c6d6df178f3ddc6dfca
dudu3000 <dudumanionut96@gmail.com>
2020-10-28 11:13:06 +0200

    Did the REST services to send JSON file after reading through HTTP Get

---
+++ Controllers/RestJsonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApiNetCore.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RestJsonController : ControllerBase
+    {
+        public string Get()
+        {
+            string json = "";
+            using (System.IO.StreamReader r = new System.IO.StreamReader("file.json"))
+            {
+                json = r.ReadToEnd();
+            }
+            return json;
+        }
+    }
+}
